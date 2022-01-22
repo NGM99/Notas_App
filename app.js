d6b4944b6189a9eee19f0744d1c2a4bfdd9527ca
NGM99 <noel_alejandro10@hotmail.com>
2022-01-22 15:55:19 -0600

    Primer commit del proyecto

---
+++ app.js
@@ -0,0 +1,39 @@
+let {
+  leerJSON,
+  escribirJSON,
+  filtrarPorEstado,
+} = require("./funcionesDeTareas");
+
+param = process.argv[2];
+
+switch (param) {
+  case "listar":
+    let dato = leerJSON();
+    dato.forEach(function (d) {
+      console.log(d.titulo);
+    });
+    break;
+
+  case "crear":
+    let titulo = process.argv[3];
+    let estado = "pendiente";
+    escribirJSON(titulo, estado);
+    break;
+
+  case "filtrar":
+    let filtro = process.argv[3];
+    filtrarPorEstado(filtro);
+    break;
+
+  case "filtarPorTitulo":
+    let filtroTitulo = process.argv[3];
+
+    break;
+
+  case undefined:
+    console.log("Atención - Tienes que pasar una acción");
+    break;
+
+  default:
+    console.log("No entiendo qué quieres hacer");
+}
---
+++ funcionesDeTareas.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+function leerJSON() {
+  let JSONData = JSON.parse(fs.readFileSync("./tareas.json", "utf-8"));
+  return JSONData;
+}
+
+function escribirJSON(titulo, estado) {
+  let JSONData = leerJSON();
+  JSONData.push({ titulo: titulo, estado: estado });
+  fs.writeFileSync("./tareas.json", JSON.stringify(JSONData, null, 2));
+}
+
+function filtrarPorEstado(filtro) {
+  let datos = leerJSON();
+  let filtrarJSON = datos.filter(function (dato) {
+    return dato.estado === filtro;
+  });
+  console.log(filtrarJSON);
+}
+
+// function filtrarPorTitulo(filtroTitulo) {
+//   let datos = leerJSON();
+//   let filtrarJSON = datos.filter(function (dato) {
+//     return dato.titulo === filtroTitulo;
+//   });
+//   console.log(filtrarJSON);
+// }
+
+module.exports = { leerJSON, escribirJSON, filtrarPorEstado };
